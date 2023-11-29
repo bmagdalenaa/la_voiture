d7b24430783b11d7cf2774c61bd87bc4955602cd
Leonardo Lourencini <leolourencini@hotmail.com>
2023-11-29 17:00:11 -0600

    Added Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do\
+  root "home#index"
+
+  get 'home/index'
   get 'vehicle/index'
   get 'vehicle/show'
   get 'type/index'
@@ -21,5 +24,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
