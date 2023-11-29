c3d00775a10d374bea174d2297bcb3e6c8edf724
bgd <delgado_belen@hotmail.com>
2023-11-29 16:39:10 -0600

    Created controllers for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  get 'vehicle/index'
+  get 'vehicle/show'
+  get 'type/index'
+  get 'type/show'
+  get 'order_detail/index'
+  get 'order_detail/show'
+  get 'model/index'
+  get 'model/show'
+  get 'model_color/index'
+  get 'model_color/show'
+  get 'manufacturer/index'
+  get 'manufacturer/show'
+  get 'fuel/index'
+  get 'fuel/show'
+  get 'color/index'
+  get 'color/show'
+  get 'contact_list/index'
+  get 'contact_list/show'
+  get 'color/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
