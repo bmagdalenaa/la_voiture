0a1b7604f8cb11d646a5732d8b8ec91efdaf7384
Leonardo Lourencini <leolourencini@hotmail.com>
2023-11-29 17:03:46 -0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,35 @@
 Rails.application.routes.draw do\
   root "home#index"
 
-  get 'home/index'
-  get 'vehicle/index'
-  get 'vehicle/show'
-  get 'type/index'
-  get 'type/show'
-  get 'order_detail/index'
-  get 'order_detail/show'
-  get 'model/index'
-  get 'model/show'
-  get 'model_color/index'
-  get 'model_color/show'
-  get 'manufacturer/index'
-  get 'manufacturer/show'
-  get 'fuel/index'
-  get 'fuel/show'
-  get 'color/index'
-  get 'color/show'
-  get 'contact_list/index'
-  get 'contact_list/show'
-  get 'color/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :home, only: %i[index]
+  resources :color, only: %i[index show]
+  resources :contact_list, only: %i[index show]
+  resources :fuel, only: %i[index show]
+  resources :manufacturer, only: %i[index show]
+  resources :model_colors, only: %i[index show]
+  resources :model, only: %i[index show]
+  resources :order_details, only: %i[index show]
+  resources :type, only: %i[index show]
+  resources :vehicle, only: %i[index show]
 
-  # Defines the root path route ("/")
-
+  # get 'home/index'
+  # get 'vehicle/index'
+  # get 'vehicle/show'
+  # get 'type/index'
+  # get 'type/show'
+  # get 'order_detail/index'
+  # get 'order_detail/show'
+  # get 'model/index'
+  # get 'model/show'
+  # get 'model_color/index'
+  # get 'model_color/show'
+  # get 'manufacturer/index'
+  # get 'manufacturer/show'
+  # get 'fuel/index'
+  # get 'fuel/show'
+  # get 'color/index'
+  # get 'color/show'
+  # get 'contact_list/index'
+  # get 'contact_list/show'
+  # get 'color/index'
 end
