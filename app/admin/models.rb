3597113faf24d23c34f38a88303a821ebee76776
Leonardo Lourencini <leolourencini@hotmail.com>
2023-12-01 09:23:07 -0600

    Added models to the admin page

---
+++ app/admin/models.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Model do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :model_name
+  permit_params :manufacturer_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:manufacturer_name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
