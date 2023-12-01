e845efd1848a082a59fcef803051b2283b246e71
bgd <delgado_belen@hotmail.com>
2023-12-01 01:30:59 -0600

    placeholder vehicle section for home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    # Placeholder data for testing
+    @featured_vehicles = [
+      OpenStruct.new(model: 'Placeholder Model 1', image_url: 'nissan-placeholder.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+      OpenStruct.new(model: 'Placeholder Model 2', image_url: 'nissan-placeholder.jpg', description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+      OpenStruct.new(model: 'Placeholder Model 3', image_url: 'nissan-placeholder.jpg', description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'),
+      OpenStruct.new(model: 'Placeholder Model 4', image_url: 'nissan-placeholder.jpg', description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.')
+    ]
   end
 end
