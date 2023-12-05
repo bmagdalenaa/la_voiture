32e84c9f8675180c19c94991bca225e325c64a4c
Leonardo Lourencini <leolourencini@hotmail.com>
2023-12-05 14:29:58 -0600

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,11 @@ require "faker"
 require "csv"
 
 # Delete data
-Color.delete_all
-Fuel.delete_all
-Type.delete_all
-Province.delete_all
+# Color.delete_all
+# Fuel.delete_all
+# Type.delete_all
+# Province.delete_all
+# Manufacturer.destroy_all
 
 # Reseed ids
 ActiveRecord::Base.connection.execute(
@@ -18,40 +19,51 @@ unless AdminUser.exists?(email: 'admin@example.com')
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 end
 
+# Import data from a csv file to Manufacturer table
+# filename_car = Rails.root.join("db/car_data.csv")
+# puts "Loading Cars from the CSV file: #{filename_car}"
+# csv_data = File.read(filename_car)
+
+# car_data = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+# car_data.each do |c|
+#   manufacturer = Manufacturer.create(manufacturer_name: c["manufacturer_name"])
+# end
+
 # Import data from a csv file to Province table
-filename = Rails.root.join("db/provinces_data.csv")
-puts "Loading Provinces from the CSV file: #{filename}"
-csv_data = File.read(filename)
-provinces = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+# filename = Rails.root.join("db/provinces_data.csv")
+# puts "Loading Provinces from the CSV file: #{filename}"
+# csv_data = File.read(filename)
+# provinces = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 
-provinces.each do |row|
-  puts "Processing row: #{row.inspect}"
-  puts "province_name: #{row['province_name']}"
-  Province.create(
-    province_name: row["province_name"],
-    pst: row["pst"].presence || 0.0,
-    gst: row["gst"].presence || 0.0,
-    hst: row["hst"].presence || 0.0,
-    total_tax_rate: row["total_tax_rate"].presence || 0.0
-  )
-end
+# provinces.each do |row|
+#   puts "Processing row: #{row.inspect}"
+#   puts "province_name: #{row['province_name']}"
+#   Province.create(
+#     province_name: row["province_name"],
+#     pst: row["pst"].presence || 0.0,
+#     gst: row["gst"].presence || 0.0,
+#     hst: row["hst"].presence || 0.0,
+#     total_tax_rate: row["total_tax_rate"].presence || 0.0
+#   )
+# end
 
-puts "Created #{Province.count} provinces."
+# puts "Created #{Province.count} provinces."
 
 # Colors
-10.times do
-  color_name = Faker::Vehicle.color
-  Color.find_or_create_by(color_name: color_name)
-end
+# 10.times do
+#   color_name = Faker::Vehicle.color
+#   Color.find_or_create_by(color_name: color_name)
+# end
 
 # Fuel
-6.times do
-  fuel_name = Faker::Vehicle.fuel_type
-  Fuel.find_or_create_by(fuel_name: fuel_name)
-end
+# 6.times do
+#   fuel_name = Faker::Vehicle.fuel_type
+#   Fuel.find_or_create_by(fuel_name: fuel_name)
+# end
 
 # Type
-10.times do
-  type_name = Faker::Vehicle.car_type
-  Type.find_or_create_by(type_name: type_name)
-end
+# 10.times do
+#   type_name = Faker::Vehicle.car_type
+#   Type.find_or_create_by(type_name: type_name)
+# end
