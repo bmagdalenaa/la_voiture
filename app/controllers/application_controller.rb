f28a4a1207902ebb74211a14c057d67d40f20581
bgd <delgado_belen@hotmail.com>
2023-12-12 14:02:16 -0600

    fix active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,25 +5,28 @@ class ApplicationController < ActionController::Base
   private
 
   def set_breadcrumbs
+    return if is_active_admin_page?
+
     add_breadcrumb 'Home', :root_path
 
     if controller_name == 'car_models'
       add_breadcrumb 'Manufacturers', manufacturer_index_path
-      # Only add this if @car_model is set and has an associated manufacturer
       add_breadcrumb @car_model.manufacturer.name, manufacturer_path(@car_model.manufacturer) if @car_model&.manufacturer
     end
 
     if controller_name == 'vehicles'
       add_breadcrumb 'Car Models', car_models
-      # Only add this if @vehicle is set and has an associated car model
       add_breadcrumb @vehicle.car_model.name, car_model_path(@vehicle.car_model) if @vehicle&.car_model
     end
 
-    # Add controller name breadcrumb unless it's the 'home' controller
     add_breadcrumb controller_name.titleize, controller_index_path unless controller_name == 'home'
-
-    # Add action name breadcrumb unless it's the 'index' action
     add_breadcrumb action_name.titleize, action_path unless action_name == 'index'
+  end
+
+  private
+
+  def is_active_admin_page?
+    request.path.start_with?('/admin')
   end
 
   # Helper to generate path for the controller's index action
