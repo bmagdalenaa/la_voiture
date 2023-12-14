c9fb2cf6e36c35f7c6482b8776b46ae71d741ae5
bgd <delgado_belen@hotmail.com>
2023-12-13 21:09:07 -0600

    debugging

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,25 +1,18 @@
-ActiveAdmin.register Manufacturer do
+ActiveAdmin.register User do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :manufacturer_name, :image
+  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   #
   # or
   #
   # permit_params do
-  #   permitted = [:manufacturer_name]
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  form do |f|
-    f.semantic_errors
-    f.inputs
-    f.inputs do
-      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size: '50x50') : ''
-    end
-    f.actions
-  end
+  
 end
--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -3,6 +3,7 @@ class CarModel < ApplicationRecord
     self.car_model_name
   end
   belongs_to :manufacturer
+  has_one_attached :image
 
   validates :car_model_name, presence: true
   validates :car_model_name, uniqueness: true
